f4307dbfdbc5f98a991669336bd3272417869fab
wilbobraggins <wilbobraggins@gmail.com>
2018-06-15 02:23:01 +0000

    comment form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -19,7 +19,8 @@ class PlacesController < ApplicationController
   end  
 
   def show
-    @place = Place.find(params[:id])  
+    @place = Place.find(params[:id]) 
+    @comment = Comment.new 
   end
 
   def edit
