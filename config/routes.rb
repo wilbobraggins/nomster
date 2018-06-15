e0031aa606bb79bc3028ff024c0beb886d3a0251
wilbobraggins <wilbobraggins@gmail.com>
2018-06-15 02:50:34 +0000

    comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
-  resources :places
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :places do
+    resources :comments, only: :create
+  end
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
